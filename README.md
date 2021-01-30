693e00bb2652c7f9edcd9da30a5f9d5463635013
Adam Szymanowski <adam.szym@yahoo.pl>
2021-01-30 11:36:13 +0100

    raylib [core] example - keyboard input (Done + extra stuff)

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Initial setup for the project:
 [Visual studio 2019 Setup](https://github.com/raysan5/raylib/wiki/Create-Visual-Studio-2019-Project)
 
 Make sure to do it if you want to compile it on your machine!
+
+# Notes
+- First example was blatantly copy-abd-pasted just to see whether inital setup actually works.
--- RayLibExamples.c
+++ RayLibExamples.c
@@ -1,41 +1,94 @@
 #include "raylib.h"
+#include <stdbool.h>
+#include <stdio.h>
 
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
+    // Screen Setup
     const int screenWidth = 800;
     const int screenHeight = 450;
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - keyboard input");
+    SetTargetFPS(60);
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    // Game setup
+    Vector2 ballPosition = { (float)screenWidth / 2, (float)screenHeight / 2 };
+    float ballRadius = 20;
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+    #define debugBufferSize 10
+    char debugBuffer[debugBufferSize] = { 0 };
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    while (!WindowShouldClose())
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
 
-        // Draw
-        //----------------------------------------------------------------------------------
+        if (IsKeyDown(KEY_LEFT))
+        {
+            float boundaryLeft = ballRadius;
+            if (ballPosition.x <= boundaryLeft)
+            {
+                ballPosition.x = boundaryLeft;
+            }
+            else
+            {
+                ballPosition.x -= 2.0f;
+            }
+        }
+
+        if (IsKeyDown(KEY_RIGHT))
+        {
+            float boundaryDown = (screenWidth - ballRadius);
+            if (ballPosition.x >= boundaryDown)
+            {
+                ballPosition.x = boundaryDown;
+            }
+            else
+            {
+                ballPosition.x += 2.0f;
+            }
+        }
+
+        if (IsKeyDown(KEY_UP))
+        {
+            float boundaryUp = ballRadius;
+            if (ballPosition.y <= boundaryUp) 
+            {
+                ballPosition.y = boundaryUp;
+            }
+            else
+            {
+                ballPosition.y -= 2.0f;
+            }
+        }
+
+        if (IsKeyDown(KEY_DOWN))
+        {
+            float boundaryDown = (screenHeight - ballRadius);
+            if (ballPosition.y >= boundaryDown)
+            {
+                ballPosition.y = boundaryDown;
+            }
+            else
+            {
+                ballPosition.y += 2.0f;
+            }
+        }
+
+        // Drawing logic in separate block for good measure.
         BeginDrawing();
+        {
+            ClearBackground(RAYWHITE);
+            DrawText("move the ball with arrow keys", 10, 10, 20, DARKGRAY);
+            DrawCircleV(ballPosition, ballRadius, MAROON);
 
-        ClearBackground(RAYWHITE);
+            // Debug drawing
+            snprintf(debugBuffer, debugBufferSize, "X: %f", ballPosition.x);
+            DrawText(debugBuffer, screenWidth - 100, 10, 20, DARKBLUE);
+            snprintf(debugBuffer, debugBufferSize, "Y: %f", ballPosition.y);
+            DrawText(debugBuffer, screenWidth-100, 30, 20, DARKBLUE);
 
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
+        }
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
+    CloseWindow();
     return 0;
 }
